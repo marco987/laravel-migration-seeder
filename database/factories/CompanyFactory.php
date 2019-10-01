b93eb47d436426b36a29d30d5314c68b30e17a43
marco987 <pacemarco87@gmail.com>
2019-10-01 17:44:34 +0200

    Correzione bug

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -7,9 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Company::class, function (Faker $faker) {
     return [
-      'name' => $faker -> words(2),
-      'phone' => $faker -> e164PhoneNumber(),
-      'mail' => $faker -> email(),
-      'website' => $faker -> domainName()
+      'name' => $faker -> name,
+      'phone' => $faker -> e164PhoneNumber,
+      'mail' => $faker -> email,
+      'website' => $faker -> domainName
     ];
 });

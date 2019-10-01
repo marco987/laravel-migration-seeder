d0d168119ecc8499f1545a611256dbbc34329048
marco987 <pacemarco87@gmail.com>
2019-10-01 17:24:29 +0200

    Seeder

---
+++ database/seeds/PlacesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Place;
+
+class PlacesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Place::class, 100) -> create();
+    }
+}

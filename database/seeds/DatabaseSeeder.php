5650c9bdc554b5dbf758aa6e31408b95cc51f504
marco987 <pacemarco87@gmail.com>
2019-10-01 17:26:09 +0200

    DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(CompaniesSeeder::class);
+        $this->call(PlacesSeeder::class);
+        $this->call(EmployeesSeeder::class);
     }
 }
